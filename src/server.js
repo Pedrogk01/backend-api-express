a27aa7f8a13f7a5b0acc3f0f97b011f60cd55e66
Pedro Teixeira <pedro.t.oliveira10@gmail.com>
2026-02-24 22:07:23 -0300

    http methods examples

--- src/server.js
+++ src/server.js
@@ -5,9 +5,29 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('ola mundo express api')
+})
+
+app.get('/user', (req, res) => {
+  res.send('exemplo de GET na rota /user')
+})
+
+app.post('/user', (req, res) => {
+  res.send('exemplo de POST na rota /user')
+})
+
+app.put('/user', (req, res) => {
+  res.send('exemplo de PUT na rota /user')
+})
+
+app.patch('/user', (req, res) => {
+  res.send('exemplo de PATCH na rota /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('exemplo de DELETE na rota /user')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`servidor rodando em http://localhost:${port}`)
 })
